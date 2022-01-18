e64c4650910e096861a312cdf7bc6fa301726548
abliger <3330181534@qq.com>
2022-01-18 20:47:54 +0800

    feat(quickstart): 拉取代码

--- src/extension.ts
+++ src/extension.ts
@@ -1,18 +1,31 @@
 // The module 'vscode' contains the VS Code extensibility API
 // Import the module and reference it with the alias vscode in your code below
 import * as vscode from 'vscode'
-import { rollup } from './rollup'
+import { Rollup } from './rollup'
 
 // this method is called when your extension is activated
 // your extension is activated the very first time the command is executed
 export function activate(context: vscode.ExtensionContext) {
   vscode.window.registerTreeDataProvider(
-    'package-dependencies',
-    new rollup()
-  );
+    'course_help',
+    new Rollup()
+  )
   context.subscriptions.push(vscode.commands.registerCommand('site.abliger.rollup_course.showRollup', () =>
     vscode.commands.executeCommand('workbench.action.openWalkthrough', 'site.abliger.rollup-course#rollup_learn')))
 
+  context.subscriptions.push(vscode.commands.registerCommand('site.abliger.rollup_course.pullProject', async() => {
+    const uri = {
+      gitee: 'https://github.com.cnpmjs.org/rollup/rollup-starter-app',
+      github: 'https://github.com/rollup/rollup-starter-app.git'
+    } as {[key:string]:string}
+    const remoteGit = await vscode.window.showQuickPick(Object.keys(uri))
+    if (!remoteGit) {
+      return
+    }
+    const a = await vscode.commands.executeCommand('git.clone', uri[remoteGit])
+    console.log(a)
+    vscode.commands.executeCommand('workbench.action.openWalkthrough', 'site.abliger.rollup-course#rollup_learn')
+  }))
 }
 
 // this method is called when your extension is deactivated
--- src/rollup.ts
+++ src/rollup.ts
@@ -1,25 +1,26 @@
-import * as vscode from 'vscode';
-
-export class rollup implements vscode.TreeDataProvider<Dependency> {
-  getTreeItem(element: Dependency): Dependency | Thenable<Dependency> {
-    return element
-  }
-  getChildren(element?: any): vscode.ProviderResult<Dependency[]> {
-    let rollup_course = new Dependency("Rollup 演示文档", vscode.TreeItemCollapsibleState.None)
-    rollup_course.command = {
-      title: 'Rollup 演示文档',
-      command: 'site.abliger.rollup_course.showRollup'
-    }
-    return [rollup_course]
-  }
-}
+import * as vscode from 'vscode'
 
 class Dependency extends vscode.TreeItem {
   constructor(
     public readonly label: string,
     public readonly collapsibleState: vscode.TreeItemCollapsibleState
   ) {
-    super(label, collapsibleState);
-    this.tooltip = `${this.label}`;
+    super(label, collapsibleState)
+    this.tooltip = `${this.label}`
+  }
+}
+
+export class Rollup implements vscode.TreeDataProvider<Dependency> {
+  getTreeItem(element: Dependency): Dependency | Thenable<Dependency> {
+    return element
+  }
+
+  getChildren(): vscode.ProviderResult<Dependency[]> {
+    const rollupCourse = new Dependency('Rollup 演示文档', vscode.TreeItemCollapsibleState.None)
+    rollupCourse.command = {
+      title: 'Rollup 演示文档',
+      command: 'site.abliger.rollup_course.showRollup'
+    }
+    return [rollupCourse]
   }
 }
